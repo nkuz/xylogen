6515114f6816dbb0807e29b0a56edba0b9ab4e17
aekang12 <aekang12@gmail.com>
2019-06-08 18:33:02 -0400

    Made Waterfall
    blue LED strip

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,52 @@
 #include <FastLED.h>
 #include <Arduino.h>
 
-FASTLED_USING_NAMESPACE
+#define NUM_LEDS 10
+int r = 193;
+int g = 217;
 
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
+CRGBArray<NUM_LEDS> leds;
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
 
-#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
-#warning "Requires FastLED 3.1 or later; check github for latest code."
-#endif
+const TProgmemPalette16 Palette PROGMEM =
+    {
+        CRGB::Gray,
+        CRGB::Aqua,
+        CRGB::Cyan,
+        CRGB::Blue,
+        CRGB::Blue,
+        CRGB::DarkBlue,
+        CRGB::DarkBlue,
+        CRGB::Black,
+};
 
-#define DATA_PIN    3
-//#define CLK_PIN   4
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
-#define NUM_LEDS    10
-CRGB leds[NUM_LEDS];
-
-#define BRIGHTNESS          96
-#define FRAMES_PER_SECOND  120
-
-void setup() {
-  delay(3000); // 3 second delay for recovery
-  
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
+void setup()
+{
+  FastLED.addLeds<NEOPIXEL, 3>(leds, NUM_LEDS);
+  currentPalette = Palette;
+  currentBlending = LINEARBLEND;
 }
 
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+    uint8_t brightness = 50;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
 
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-//SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
-uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-  
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  //gPatterns[gCurrentPatternNumber]();
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
-
-  // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
+    
+    FillLEDsFromPaletteColors( startIndex);
+    
+    FastLED.show();
+    FastLED.delay(10 );
 }
 
-
-
-void rainbow() 
-{
-  // FastLED's built-in rainbow generator
-  fill_rainbow( leds, NUM_LEDS, gHue, 7);
-}
 
